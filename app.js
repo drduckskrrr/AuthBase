a4040826a2974179dd2f070d5523c2ffbb74e623
viethmhe150098 <mrkai6996@gmail.com>
2022-03-16 21:55:59 +0700

    V2

---
+++ app.js
@@ -0,0 +1,77 @@
+//jshint esversion:6
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+const encrypt = require('mongoose-encryption')
+
+
+const app = express()
+
+mongoose.connect('mongodb://localhost:27017/userDB')
+
+const userSchema = new mongoose.Schema( {
+    email: String,
+    password: String
+})
+
+userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields: ['password']})
+
+const User = mongoose.model('User', userSchema)
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+app.route('/login')
+    .get((req, res) => {
+        res.render('login')
+    })
+    .post((req, res) => {
+        const username = req.body.username
+        const password = req.body.password
+        User.findOne({
+            //mongoose decrypt the password automatically
+            email: username
+        }, (err, doc) => {
+            if (err) {
+                console.log(err)
+            } else {
+                if (doc) {
+                    if (doc.password === password) {
+                        res.render('secrets')
+                    }
+                }
+
+            }
+        })
+    })
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+    newUser.save((err) => {
+        //mongoose encrypt the password automatically
+        if (!err) {
+            res.render('secrets')
+        } else {
+            console.log(err)
+        }
+    })
+})
+
+
+
+app.listen(3000, () => {
+    console.log('listening on port 3000')
+})
